a67aa5a85f4c95b23c50f6192966d154b6daace7
WonYoungYun <asmar20288@naver.com>
2018-11-13 22:36:50 +0900

    weather

--- smp2/js/app.js
+++ smp2/js/app.js
@@ -1,9 +1,9 @@
 import user from './components/user.js'
 import clock from './components/clock.js'
-// import weather from './components/weather.js'
+import weather from './components/weather.js'
 
 document.addEventListener("DOMContentLoaded", ()=>{
     user.init()
     clock.init()
-    // weather.init()
+    weather.init()
 })
--- smp2/js/components/clock.js
+++ smp2/js/components/clock.js
@@ -1,23 +1,22 @@
 const tag = '[time]'
 const location = document.getElementsByClassName('location')
 const time = location[0].getElementsByClassName('time')
+
+function getTime(){
+    function timeTrans(time){
+       return time<10 ? `0${time}` : time
+    }
+    const date = new Date()
+    const hour = date.getHours()
+    const minute = date.getMinutes()
+    const second = date.getSeconds()
+    time[0].innerText = `${timeTrans(hour)}:${timeTrans(minute)}:${timeTrans(second)}`
+
+ }
 export default{
-
-   getTime(){
-      function timeTrans(time){
-         return time<10 ? `0${time}` : time
-      }
-      const date = new Date()
-      const hour = date.getHours()
-      const minute = date.getMinutes()
-      const second = date.getSeconds()
-      time[0].innerText = `${timeTrans(hour)}:${timeTrans(minute)}:${timeTrans(second)}`
-
-   },
-
    init(){
-      this.getTime()
-      setInterval(this.getTime, 1000)
+      getTime()
+      setInterval(getTime, 1000)
    }
 
 
