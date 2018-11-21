4d41a0f15973e08c06f1d5cb05b3c19f0e652a9b
WonYoungYun <asmar20288@naver.com>
2018-11-21 21:23:10 +0900

    webpage

---
+++ smp3/js/footer.js
@@ -0,0 +1,30 @@
+const info_title = document.querySelectorAll(".info_copyright .info_title")
+
+
+function copyright_display() {
+    info_title.forEach(el => {
+        let info_detail = el.nextElementSibling
+        let icon = el.querySelector("i")
+        el.addEventListener("click", () => {
+            if (info_detail.style.display === "block") {
+                info_detail.style.display = "none"
+
+                icon.classList.remove("fa-angle-up")
+                icon.classList.add("fa-angle-down")
+
+            } else {
+                info_detail.style.display = "block"
+                height = info_detail.clientHeight * -1
+                info_detail.style.top = `${height-10}px`
+                icon.classList.remove("fa-angle-down")
+                icon.classList.add("fa-angle-up")
+            }
+
+        })
+    })
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    copyright_display()
+})
