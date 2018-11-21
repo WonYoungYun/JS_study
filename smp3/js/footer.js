e818b15fae45106d67325404bfd0531854092415
WonYoungYun <asmar20288@naver.com>
2018-11-21 21:29:04 +0900

    web

--- smp3/js/footer.js
+++ smp3/js/footer.js
@@ -1,6 +1,19 @@
 const info_title = document.querySelectorAll(".info_copyright .info_title")
 
 
+
+function check_display() {
+    info_title.forEach(el => {
+        let target_detail = el.nextElementSibling
+        let span = el.querySelector("i")
+        if (span.classList.contains("fa-angle-up")) {
+            target_detail.style.display = "none"
+            span.classList.remove("fa-angle-up")
+            span.classList.add("fa-angle-down")
+        }
+    })
+}
+
 function copyright_display() {
     info_title.forEach(el => {
         let info_detail = el.nextElementSibling
@@ -13,6 +26,7 @@ function copyright_display() {
                 icon.classList.add("fa-angle-down")
 
             } else {
+                check_display()
                 info_detail.style.display = "block"
                 height = info_detail.clientHeight * -1
                 info_detail.style.top = `${height-10}px`
