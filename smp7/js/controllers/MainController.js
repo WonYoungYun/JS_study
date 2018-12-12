8dff21ebda81d3bb29bb4f8087da1cebea5d5e5b
WonYoungYun <asmar20288@naver.com>
2018-12-13 01:47:44 +0900

    today

--- smp7/js/controllers/MainController.js
+++ smp7/js/controllers/MainController.js
@@ -1,9 +1,11 @@
 import FormView from '../views/FormView.js'
 import ResultView from '../views/ResultView.js'
 import TabView from '../views/TabView.js'
+import ListView from '../views/ListView.js'
+
 
 import SearchModel from '../models/SearchModel.js';
-
+import ListModel from '../models/ListModel.js'
 
 const tag = '[MainController]'
 
@@ -15,16 +17,31 @@ export default {
 
         TabView.setup(document.querySelector('.content-nav')).on('@change', e => this.onChangeTab(e.detail.tabName))
 
+        ListView.setup(document.querySelector('.movie-list')).on('@click', e => this.onClickMovieList(e.detail.keyword))
 
         ResultView.setup(document.querySelector('.search-result'))
 
         this.selectedTab = '영화 목록'
         this.renderView()
     },
+
     renderView() {
         TabView.setSelectTab(this.selectedTab)
+        if (this.selectedTab === '영화 목록') {
+            this.fetchSearchMovieList()
+        } else {
+            ListView.hide()
+        }
+
         ResultView.hide()
     },
+
+    fetchSearchMovieList() {
+        ListModel.list().then(data => {
+            ListView.render(data)
+        })
+    },
+
     search(query) {
         FormView.setValue(query)
         SearchModel.list(query).then(data => {
@@ -46,5 +63,8 @@ export default {
         this.selectedTab = tabName
         this.renderView()
     },
+    onClickMovieList(keyword) {
+        this.search(keyword)
+    }
 
 }
